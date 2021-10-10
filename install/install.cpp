c056160f8cf18c7fc9ca0fb1e043a6069aa476f1
jhenrique09 <jhsv09@gmail.com>
2021-10-10 15:33:05 -0300

    recovery: Disable downgrade check
    Change-Id: I734fad12a54a232a00240ba105a79828135896a9

--- install/install.cpp
+++ install/install.cpp
@@ -175,7 +175,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
   }
 
   // Check for downgrade version.
-  bool undeclared_downgrade = false;
+  /*bool undeclared_downgrade = false;
   int64_t build_timestamp =
       android::base::GetIntProperty("ro.build.date.utc", std::numeric_limits<int64_t>::max());
   int64_t pkg_post_timestamp = 0;
@@ -200,7 +200,7 @@ static bool CheckAbSpecificMetadata(const std::map<std::string, std::string>& me
   if (undeclared_downgrade &&
       !(ui->IsTextVisible() && ask_to_continue_downgrade(ui->GetDevice()))) {
     return false;
-  }
+  }*/
 
   return true;
 }
